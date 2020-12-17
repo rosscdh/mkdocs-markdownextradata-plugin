ac1d5df4a2f41b4d99be7c94d6376daba6894105
Michael Jess <michael.jess@sap.com>
2020-12-17 14:36:28 +0100

    Another attempt at resolving nav/page title issues (see #20)

--- test/docs/index.md
+++ test/docs/index.md
@@ -1,5 +1,3 @@
-# Hi there, {{ customer.name }}
-
 Welcome to {{ customer.web_url }}
 
 Inside the included md file there 3 {{ star }}
--- test/test_basic.py
+++ test/test_basic.py
@@ -22,7 +22,7 @@ def test_basic_working():
         assert index_file.exists(),  f"{index_file} does not exist, it should"
         contents = index_file.read_text()
 
-        assert '<h1 id="hi-there-your-name-here">Hi there, Your name here</h1>' in contents, f"customer.name is not in index"
+        assert '<a href="page/" class="nav-link">Hi there, Your name here</a>' in contents, f"customer.name is not in index"
         assert '<p>Inside the included md file there 3 <img alt="star" src="ressources/star.png" /></p>' in contents, f"customer.star is not in index or not rendering as expected"
         assert f"Welcome to {customer.get('web_url')}" in contents, f"customer.web_url is not in index"
         assert f"{{#binding.path}}" in contents, f"Jinja2 comment syntax wasn't reconfigured via jinja_options as expected"
