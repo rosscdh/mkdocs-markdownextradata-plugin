d732823e0344e723d2ef2ae3dad99dc71cbf93fb
Jeremy Goh <jeremy_gsk@outlook.com>
2023-01-27 00:53:09 +0800

    chore: py3 only classifier

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
