23eea4aa65971b33c3718fef08ccd8cb2f84b7dd
Jeremy Goh <30731072+thatlittleboy@users.noreply.github.com>
2024-08-03 18:22:13 +0800

    migrate to gh actions, add tests and publish wheel (#44)
    * migrate to gh actions, add tests and publish wheel
    * downgrade ubuntu
    * drop explicit support for py3.4 and py3.5 since tests are using fstrings
    * revert back to actual pypi
    * remove tests in setup.py
    * chore: py3 only classifier

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import os
-import sys
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
+
 
 def read(fname):
     file_path = os.path.join(os.path.dirname(__file__), fname)
@@ -9,22 +8,10 @@ def read(fname):
         content = file.read()
     return content if content else 'no content read'
 
-class PyTest(TestCommand):
-    user_options = []
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 setup(
     name='mkdocs-markdownextradata-plugin',
-    version='0.2.4',
+    version='0.2.5',
     description='A MkDocs plugin that injects the mkdocs.yml extra variables into the markdown template',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
@@ -33,14 +20,7 @@ setup(
     author='Ross Crawford-d\'Heureuse',
     author_email='sendrossemail@gmail.com',
     license='MIT',
-    tests_require=[
-        'pytest',
-        'mkdocs',
-        'pyyaml',
-        'click',
-    ],
-    cmdclass = {'test': PyTest},
-    python_requires='>=2.7.9,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    python_requires='>=3.6',
     install_requires=[
         'mkdocs',
         'pyyaml',
@@ -51,13 +31,13 @@ setup(
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     packages=find_packages(exclude=['*.tests']),
     entry_points={
